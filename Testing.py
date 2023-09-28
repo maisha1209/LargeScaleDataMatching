3e200d4522cdcb19648b532205c3d78963d3d794
maisha1209 <143038836+maisha1209@users.noreply.github.com>
2023-09-28 14:44:46 -0400

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -1,7 +1,39 @@
 import gzip
+from typing import List
 
-with gzip.open('Papers.txt.gz','rb') as f:
-    file_content=f.readlines(1000)
-    for line in file_content:
-        print(line)
-     
+
+        
+
+class Paper:
+ def __init__(self, mag_paper_id, doi_number, title):
+        self.mag_paper_id = mag_paper_id
+        self.doi_number = doi_number
+        self.title = title
+
+
+paper_list = []
+def create_paper(line):
+
+    fields = line.strip().split('\t')
+   
+    mag_paper_id = fields[0]
+    doi_number = fields[2]
+    title = fields[4]
+
+    return Paper(mag_paper_id, doi_number, title)
+
+
+#INSIDE LOOP -> 
+#for line in file:
+
+ #paper_obj = create_paper(line)
+#paper_list.append(paper_obj)
+
+with gzip.open('Papers.txt.gz','rt', encoding='utf-8') as f:
+   # file_content=f.readlines()
+    for line in f:
+        print("paper_objcreatestart")
+        paper_obj = create_paper(line)
+        print("paper_objcreateend")
+        paper_list.append(paper_obj)
+        #print(paper_list[0].mag_paper_id, paper_list[0].doi_number,paper_list[0].title)        
